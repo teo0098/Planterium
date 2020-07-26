cdc748075fed675e48d3c8a4bc0cc485efcd48bf
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2020-07-26 12:53:18 +0200

    Shorten lines of codes

--- src/Components/Inputs/RepeatPassword/RepeatPassword.tsx
+++ src/Components/Inputs/RepeatPassword/RepeatPassword.tsx
@@ -5,17 +5,15 @@ import { Field } from 'react-final-form';
 
 import inputStyles from '../../../tsStyleSettings/inputStyles';
 
-const RepeatPassword : React.FC = () => {
-    return (
-        <Field name="rpassword" component="input">
-            {({ input, meta }) => (
-                <div>
-                    <TextField type="password" style={inputStyles} {...input} label="Repeat password" variant="filled" />
-                    {meta.error && meta.touched && <Alert style={{ textAlign: 'left' }} severity="error"> {meta.error} </Alert>}
-                </div>
-            )}
-        </Field>
-    )
-}
+const RepeatPassword : React.FC = () => (
+    <Field name="rpassword" component="input">
+        {({ input, meta }) => (
+            <div>
+                <TextField type="password" style={inputStyles} {...input} label="Repeat password" variant="filled" />
+                {meta.error && meta.touched && <Alert style={{ textAlign: 'left' }} severity="error"> {meta.error} </Alert>}
+            </div>
+        )}
+    </Field>
+)
 
 export default RepeatPassword;
